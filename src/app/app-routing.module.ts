6c28e8d7daa9dac95f96eead9365bffac383bca1
canercalskan <cvn3r8@gmail.com>
2023-03-11 23:41:13 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/pages/home/home.component';
+import { JoinComponent } from './components/pages/join/join.component';
+const routes: Routes = [
+  {path : '' , pathMatch : 'prefix', redirectTo : 'join'},
+  {path : 'join' , component : JoinComponent},
+  {path : 'home' , component : HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
